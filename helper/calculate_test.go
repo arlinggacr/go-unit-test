ace97697965771ade3df697fb50f023be655f407
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2024-12-10 13:31:48 +0700

    add; adding error and fatal for changer from fail and failnow

--- helper/calculate_test.go
+++ helper/calculate_test.go
@@ -1,10 +1,16 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestCalculateData(t *testing.T) {
 	result := CalculateData(1, 1)
-	if result != 2 {
-		panic("result must be 2")
+	if result != 3 {
+		// t.Fail()
+		t.Error("Calculate data must be 3")
 	}
+
+	fmt.Println("test CalculateData Done")
 }
--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,10 +1,16 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHello(t *testing.T) {
 	result := HelloWorld("ara")
-	if result != "hello ara" {
-		panic("result must be hello ara")
+	if result != "hello data" {
+		// t.FailNow()
+		t.Fatal("Result Must be hello data")
 	}
+
+	fmt.Println("Test Hello Done")
 }
